d743672a45179a354c05571ddde158bd0014f482
Arunava Debnath <arunavadebnath23@gmail.com>
2023-05-29 18:17:13 +0530

    Delete the extra files.

--- index.js
+++
@@ -1,6 +0,0 @@
-const toggleButton = document.getElementById('toggle-button');
-const sideNav = document.getElementById('side-nav');
-
-toggleButton.addEventListener('click', () => {
-	sideNav.classList.toggle('show');
-});
