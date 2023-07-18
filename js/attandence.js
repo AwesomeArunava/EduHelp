182a337e95c0c4b66ccae192f555ffcaf5d32b09
Arunava Debnath <arunavadebnath23@gmail.com>
2023-07-18 12:37:26 +0530

    student list and excel work is done

--- js/attandence.js
+++ js/attandence.js
@@ -23,7 +23,7 @@
    const userId = localStorage.getItem('userId');
    console.log(userId);
    var targetDiv = document.getElementById('v-pills-tab');
-
+   var data = [];
    // Assuming the URL is: https://example.com/page?param1=value1&param2=value2
 
 // Get the current URL
@@ -37,72 +37,343 @@ console.log(classCode);
 
 
 // student list show
-document.addEventListener('DOMContentLoaded', function() {
-  const dbRef = ref(getDatabase());
-  get(child(dbRef, 'teachers')).then((snapshot) => {
+// document.addEventListener('DOMContentLoaded', function() {
+//   const dbRef = ref(getDatabase());
+//   get(child(dbRef, 'teachers')).then((snapshot) => {
+//     if (snapshot.exists()) {
+//       const teachersData = snapshot.val();
+//       var classroomData = teachersData[userId].classrooms;
+
+//       for (const key in classroomData) {
+//         if (classroomData.hasOwnProperty(key)) {
+//           console.log(classroomData[classCode]);
+//           const element = classroomData[classCode];
+//           // console.log(element);
+//           const studensList = element.students;
+//           // console.log('User Id is:', studensList);
+//           for(const key in studensList){
+            
+//             var studentId = studensList[key].studentUID;
+//             console.log(studentId);
+//             //student name list
+//             const dbRef = ref(getDatabase());
+//             get(child(dbRef, 'users')).then((snapshot) => {
+//               if (snapshot.exists()) {
+//                 const studentsData = snapshot.val();
+//                 var studentName = studentsData[studentId].studentInfo.student_name;
+//                 console.log(studentName);
+
+//                               // Inside the loop
+// var b = document.createElement('div');
+// // b.classList.add('col-sm-6', 'mt-3');
+
+// // Generate a unique key using `key` and timestamp
+// var uniqueKey = key + '-' + Date.now();
+
+// // Generate unique IDs for the elements within each div
+// var studentNameId = "StudentName-" + uniqueKey;
+
+
+// // Set the card content
+// b.innerHTML = ` <div class="card">
+//   <div class="card-body">
+//     <h5 class="card-title" id="${studentNameId}"></h5>
+    
+//   </div>
+// </div>`;
+
+// // Append the card to the target <div>
+// targetDiv.appendChild(b);
+
+// // Set the value of the elements within the current div
+// document.getElementById(studentNameId).textContent = studentName;
+
+//  }
+//             })
+            
+//         }
+//         break;
+//         }
+        
+//       }
+//     } else {
+//       console.log('No data available');
+//     }
+//   }).catch((error) => {
+//     console.error(error);
+//   });
+// });
+
+document.addEventListener('DOMContentLoaded', async function() {
+  try {
+    const dbRef = ref(getDatabase());
+    const snapshot = await get(child(dbRef, 'teachers'));
+    
     if (snapshot.exists()) {
       const teachersData = snapshot.val();
-      var classroomData = teachersData[userId].classrooms;
-
+      const classroomData = teachersData[userId].classrooms;
+      
       for (const key in classroomData) {
         if (classroomData.hasOwnProperty(key)) {
-          console.log(classroomData[classCode]);
-          const element = classroomData[classCode];
-          // console.log(element);
+          console.log(classroomData[key]);
+          const element = classroomData[key];
           const studensList = element.students;
-          // console.log('User Id is:', studensList);
-          for(const key in studensList){
-            
-            var studentId = studensList[key].studentUID;
-            // console.log(studentId.studentUID);
-            //student name list
-            const dbRef = ref(getDatabase());
-            get(child(dbRef, 'users')).then((snapshot) => {
-              if (snapshot.exists()) {
-                const studentsData = snapshot.val();
-                var studentName = studentsData[studentId].studentInfo.student_name;
-                console.log(studentName);
-
-                              // Inside the loop
-var b = document.createElement('div');
-// b.classList.add('col-sm-6', 'mt-3');
-
-// Generate a unique key using `key` and timestamp
-var uniqueKey = key + '-' + Date.now();
-
-// Generate unique IDs for the elements within each div
-var studentNameId = "StudentName-" + uniqueKey;
-
-
-// Set the card content
-b.innerHTML = ` <div class="card">
-  <div class="card-body">
-    <h5 class="card-title" id="${studentNameId}"></h5>
-    
-  </div>
-</div>`;
-
-// Append the card to the target <div>
-targetDiv.appendChild(b);
-
-// Set the value of the elements within the current div
-document.getElementById(studentNameId).textContent = studentName;
-
-              }
-            })
           
-        }
-        break;
+          for (const studentKey in studensList) {
+            const studentId = studensList[studentKey].studentUID;
+            console.log(studentId);
+            
+            const studentsSnapshot = await get(child(dbRef, 'users'));
+            if (studentsSnapshot.exists()) {
+              const studentsData = studentsSnapshot.val();
+              const studentName = studentsData[studentId].studentInfo.student_name;
+              console.log(studentName);
+              
+              // Inside the loop
+              const b = document.createElement('div');
+              const uniqueKey = key + '-' + Date.now();
+              const studentNameId = "StudentName-" + uniqueKey;
+              
+              b.innerHTML = `<div class="card">
+                <div class="card-body">
+                  <h5 class="card-title" id="${studentNameId}"></h5>
+                </div>
+              </div>`;
+              
+              // Append the card to the target <div>
+              targetDiv.appendChild(b);
+              
+              // Set the value of the elements within the current div
+              document.getElementById(studentNameId).textContent = studentName;
+            }
+          }
+          break;
         }
       }
     } else {
       console.log('No data available');
     }
-  }).catch((error) => {
+  } catch (error) {
     console.error(error);
-  });
+  }
 });
 
+
+
+// const downloadData = document.getElementById("downloadData");
+// downloadData.addEventListener("click", ()=>{
+  
+//   // alert("thik ache");
+//     const dbRef = ref(getDatabase());
+//     get(child(dbRef, 'teachers')).then((snapshot) => {
+//       if (snapshot.exists()) {
+//         const teachersData = snapshot.val();
+//         var classroomData = teachersData[userId].classrooms;
+  
+//         for (const key in classroomData) {
+//           if (classroomData.hasOwnProperty(key)) {
+//             console.log(classroomData[classCode]);
+//             const element = classroomData[classCode];
+//             // console.log(element);
+//             const studensList = element.students;
+//             // console.log('User Id is:', studensList);
+//             for(const key in studensList){
+              
+//               var studentId = studensList[key].studentUID;
+//               // console.log(studentId.studentUID);
+//               //student name list
+//               const dbRef = ref(getDatabase());
+//               get(child(dbRef, 'users')).then((snapshot) => {
+//                 if (snapshot.exists()) {
+//                   const studentsData = snapshot.val();
+//                   var studentName = studentsData[studentId].studentInfo;
+//                   console.log(studentName);
+//                   var data = studentName;
+                
+//                   // Create an array of arrays from the object data
+//                   var dataArray = data.map(obj => Object.values(obj));
+                  
+//                   // Create a new workbook and worksheet
+//                   var workbook = XLSX.utils.book_new();
+//                   var worksheet = XLSX.utils.aoa_to_sheet(dataArray);
+                  
+//                   // Add the worksheet to the workbook
+//                   XLSX.utils.book_append_sheet(workbook, worksheet, "Sheet1");
+                  
+//                   // Convert the workbook to a binary Excel file
+//                   var excelBuffer = XLSX.write(workbook, { bookType: "xlsx", type: "array" });
+                  
+//                   // Function to download the Excel file
+//                   function downloadExcel() {
+//                     var blob = new Blob([excelBuffer], { type: "application/octet-stream" });
+//                     saveAs(blob, "data.xlsx");
+//                   }
+//                   downloadExcel();
+
+  
+//                 }
+//               })
+            
+//           }
+//           break;
+//           }
+//         }
+//       } else {
+//         console.log('No data available');
+//       }
+//     }).catch((error) => {
+//       console.error(error);
+//     });
+
+// });
+
+// const downloadData = document.getElementById("downloadData");
+// downloadData.addEventListener("click", () => {
+//   try{
+//   const dbRef = ref(getDatabase());
+//   get(child(dbRef, 'teachers')).then((snapshot) => {
+//     if (snapshot.exists()) {
+//       const teachersData = snapshot.val();
+//       var classroomData = teachersData[userId].classrooms;
+
+//       for (const key in classroomData) {
+//         if (classroomData.hasOwnProperty(key)) {
+//           // console.log(classroomData[classCode]);
+//           const element = classroomData[classCode];
+//           // console.log(element);
+//           const studensList = element.students;
+//           // console.log('User Id is:', studensList);
+//           for (const key in studensList) {
+//             var studentId = studensList[key].studentUID;
+//             //student name list
+//             const dbRef = ref(getDatabase());
+//             get(child(dbRef, 'users')).then((snapshot) => {
+//               if (snapshot.exists()) {
+//                 const studentsData = snapshot.val();
+//                 var studentDeatails = studentsData[studentId].studentInfo;
+//                 console.log(studentDeatails);
+
+//                 // Convert studentName object to an array
+               
+//                 // data.push(studentName);
+               
+
+//                 // Create an array of arrays from the object data
+              
+//               }
+//             });
+//           }
+//           // excal(data);
+//           break;
+//         }
+//       }
+//     } else {
+//       console.log('No data available');
+//     }
+//   }).catch((error) => {
+//     console.error(error);
+//   });
+// } catch(error){
+//   console.log(error);
+// }
+// });
+
+const downloadData = document.getElementById("downloadData");
+downloadData.addEventListener("click", async () => {
+  try {
+    const dbRef = ref(getDatabase());
+    const snapshot = await get(child(dbRef, 'teachers'));
+
+    if (snapshot.exists()) {
+      const teachersData = snapshot.val();
+      const classroomData = teachersData[userId].classrooms;
+      const data = [];
+
+      for (const key in classroomData) {
+        if (classroomData.hasOwnProperty(key)) {
+          const element = classroomData[key];
+          const studensList = element.students;
+
+          for (const studentKey in studensList) {
+            const studentId = studensList[studentKey].studentUID;
+
+            const studentsSnapshot = await get(child(dbRef, 'users'));
+            if (studentsSnapshot.exists()) {
+              const studentsData = studentsSnapshot.val();
+              const studentDetails = studentsData[studentId].studentInfo;
+              console.log(studentDetails);
+
+              data.push(studentDetails); // Add studentDetails to the data array
+            }
+          }
+          break;
+        }
+      }
+
+      // Now, you can call the function to handle the data (e.g., create Excel file)
+      excal(data);
+    } else {
+      console.log('No data available');
+    }
+  } catch (error) {
+    console.error(error);
+  }
+});
+
+
+function excal(data){
+var dataArray = data.map(obj => Object.values(obj));
+
+// Create a new workbook and worksheet
+var workbook = XLSX.utils.book_new();
+var worksheet = XLSX.utils.aoa_to_sheet(dataArray);
+
+// Add the worksheet to the workbook
+XLSX.utils.book_append_sheet(workbook, worksheet, "Sheet1");
+
+// Convert the workbook to a binary Excel file
+var excelBuffer = XLSX.write(workbook, { bookType: "xlsx", type: "array" });
+
+// Function to download the Excel file
+function downloadExcel() {
+  var blob = new Blob([excelBuffer], { type: "application/octet-stream" });
+  saveAs(blob, "data.xlsx");
+}
+
+downloadExcel();
+}
+
+
+
+                // Assume you have an object containing data
+                // var data = [
+                //   { name: "John Doe", age: 25, email: "john@example.com" },
+                //   { name: "Jane Smith", age: 30, email: "jane@example.com" }
+                // ];
+                // var data = studentsData;
+                
+                // // Create an array of arrays from the object data
+                // var dataArray = data.map(obj => Object.values(obj));
+                
+                // // Create a new workbook and worksheet
+                // var workbook = XLSX.utils.book_new();
+                // var worksheet = XLSX.utils.aoa_to_sheet(dataArray);
+                
+                // // Add the worksheet to the workbook
+                // XLSX.utils.book_append_sheet(workbook, worksheet, "Sheet1");
+                
+                // // Convert the workbook to a binary Excel file
+                // var excelBuffer = XLSX.write(workbook, { bookType: "xlsx", type: "array" });
+                
+                // // Function to download the Excel file
+                // function downloadExcel() {
+                //   var blob = new Blob([excelBuffer], { type: "application/octet-stream" });
+                //   saveAs(blob, "data.xlsx");
+                // }
+                
+                // Attach click event to the download button
+                
+                // downloadData.addEventListener("click", downloadExcel);
 // For Card show on click
 
 const showCardButton = document.getElementById('show-card-button');
@@ -118,3 +389,4 @@ showCardButton.addEventListener("click", () => {
     profileCard.style.display = "none";
   }
 });
+
